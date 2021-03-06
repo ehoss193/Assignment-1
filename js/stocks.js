f32bb2845ed2ba6a31ba4c64310e1b2fbd6b235f
ehoss193 <erichhoss@gmail.com>
2021-03-06 15:46:50 -0700

    Local storage implemented

--- js/stocks.js
+++ js/stocks.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //API Links
+    const companyAPI = 'https://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php';
     //localStorage 
     //stores key-value pairs. So to store a entire javascript object we need to serialize it first (with JSON.stringify, for example): 
     //You will need to check to see if local storage exists before you put everything into the array
-    
-    //API Links
-    const companyAPI = 'https://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php';
+    let localStorage = window.localStorage;
+    function addStorage(company) {
+        localStorage.setItem(`${company.symbol}`, `${JSON.stringify(company)}`);
+    }
+    function retrieveStorage() {
+        keys = Object.keys(localStorage);
+        for (let x = 0; x < keys.length; x++) {
+            loadedCompanies.push(JSON.parse(localStorage.getItem(keys[x])));
+        }
+        //Sorting companies by symbol following load from local storage
+        loadedCompanies = loadedCompanies.sort(function (a, b) {
+            if (a.symbol < b.symbol) {
+                return -1
+            }
+            if (a.symbol > b.symbol) {
+                return 1;
+            }
+            return 0;
+        });
+        populateList(loadedCompanies);
+    }
+
     //Header - Credits
 
     //List of Companies
-    let loadedCompanies = new Array;
-    let options = "";
     function fetchDataCompanies() {
         fetch(companyAPI)
             .then((resp) => resp.json())
@@ -17,19 +36,18 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => console.error(error));
     }
     //will need conditional statement for stored or not stored and if loadedCompanies is empty/full
-    function storeCompanies(companies){
-    for(let company of companies)
-      {
-        loadedCompanies.push(company);
-        console.log(company);
-      }
-      populateList(loadedCompanies);
+    function storeCompanies(companies) {
+        for (let company of companies) {
+            loadedCompanies.push(company);
+            addStorage(company);
+            console.log(company);
+        }
+        populateList(loadedCompanies);
     }
-    fetchDataCompanies();
-    function populateList(companies){
+    function populateList(companies) {
         //Empty any visible options first
-        document.getElementById("companies").innerHTML ="";
-        for(company of companies){
+        document.getElementById("companies").innerHTML = "";
+        for (company of companies) {
             let option = document.createElement('option');
             let symbol = document.createAttribute("id");
             symbol.value = `${company.symbol}`;
@@ -38,27 +56,26 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("companies").add(option);
             //Attribute will be company symbol to be used in search function later
         }
-         //Options available after the list has been loaded
-         eventListenerOptions();
+        //Options available after the list has been loaded
+        eventListenerOptions();
     }
     //Event listener for filter
-    document.getElementById("go").addEventListener("click", function(){
+    document.getElementById("go").addEventListener("click", function () {
         //Filter Function
         let re = document.getElementById("field").value;
         const regexEx = loadedCompanies.filter(company => company.name.match(re));
         populateList(regexEx);
     });
     //Event listener for clear
-    document.getElementById("clear").addEventListener("click", function(){
+    document.getElementById("clear").addEventListener("click", function () {
         populateList(loadedCompanies);
     });
 
     //Event listener for option select
-    function eventListenerOptions(){
+    function eventListenerOptions() {
         options = document.querySelectorAll('option');
-        for(option of options)
-        {
-            option.addEventListener("click", function(e){
+        for (option of options) {
+            option.addEventListener("click", function (e) {
                 selectedOption = e.target;
                 //Send this option.id to find function
                 //find function sends array object to all of the other functions to populate the site
@@ -79,8 +96,17 @@ document.addEventListener("DOMContentLoaded", function () {
     //Charts
 
     //Company Name + Symbol
-    
+
     //Financials
 
-    
+    //Active code
+    //Conditional to check for storage if storage is null fetch companies from API, if storage is not null then retrieve local storage.
+    let loadedCompanies = new Array;
+    let options = "";
+    if (localStorage.length == 0) {
+        fetchDataCompanies();
+    }
+    else {
+        retrieveStorage();
+    }
 });
