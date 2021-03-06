e240d9b2b7ac9ee399c0ac9c563656d5dfa930cb
ehoss193 <erichhoss@gmail.com>
2021-03-06 09:21:33 -0700

    more styling

---
+++ js/stocks.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+//API Links
+
+//Header - Credits
+
+//List of Companies
+
+//Company Information
+
+//Map
+
+//Stock Data
+
+//Additional Information
+
+//Charts
+
+//Company Name + Symbol
+
+//Financials
+
+
+});
